d47d583d5a7fd904ae385eca1c51ff0ee50027a6
IridescentBlaze <iridescentblazer@gmail.com>
2026-04-19 21:25:37 -0400

    Created anaylsis questions (no answers yet)

---
+++ sql_sets/data_analysis.sql
@@ -0,0 +1,19 @@
+-- 1. Who are the best players overall? Who are the worst?
+
+-- 2. What players have an overall ratio greater than 1?
+
+-- 3. What are the average wins per game?
+
+-- 4. Which game has the highest average win rate? 
+
+-- 5. Who is the best player per game?
+
+-- 6. What players have a ratio in one game that is double their ratio in another? 
+
+-- 7. Based on total matches, what game is played the most?  
+
+-- 8. What players have never lost in at least one game? 
+
+-- 9. Who has the greatest shot accuracy in Battleship? (shots_hit / total_shots) 
+
+-- 10. Who plays the safest in Blackjack? (the most stands)
